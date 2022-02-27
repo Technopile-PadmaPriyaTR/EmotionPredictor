5f79d852acebac156e08496fab348503e39a5a3f
Padmesh G N <85836583+Padmesh-Coder0922@users.noreply.github.com>
2022-02-27 19:42:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,42 +14,29 @@ def home():
 def upload():
     if request.method == 'POST':
         upload_image = request.files['upload_image']
+        print(type(upload_image))
         KEY = "aec15ff556c449488f778af556ac567c"
         ENDPOINT = "https://facialemotion1.cognitiveservices.azure.com//face/v1.0/detect"
         face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-        image_stream=open(upload_image.filename,"rb")
-        #single_image_name = os.path.basename(upload_image)
+        test_image_array=upload_image
         headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': KEY}
         params = {'returnFaceId': 'true','returnFaceLandmarks': 'true','returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion'}
-        response= requests.post(ENDPOINT, params=params, headers=headers, data=image_stream)
+        response= requests.post(ENDPOINT, params=params, headers=headers, data=test_image_array)
         response.raise_for_status()
         faces = response.json()
-        image_orig = open(upload_image.filename, "rb").read()
-        img = Image.open(BytesIO(image_orig))
-        draw = ImageDraw.Draw(img)
         for face in faces:
             fa = face["faceAttributes"]
             emotion = fa["emotion"]
             values = list(emotion.values())
             keys = list(emotion.keys())
             maximum = max(values)
-            result = "Emotion: " + keys[values.index(maximum)]
-            draw.rectangle(getRectangle(face), outline='red')
-            font = ImageFont.truetype("arial.ttf", 30, )
-            draw.text((getRectangle(face)[0], getRectangle(face)[1]), result, "white", font=font, weight="bold")
-            img.show()
-        return render_template("Upload.html")
+            result = "The emotion of the person is : "+keys[values.index(maximum)]
+    return render_template("Name.html", result=result)
 
-#@app.route("/Upload")
-def getRectangle(faceDictionary):
-    rect = faceDictionary["faceRectangle"]
-    left = rect["left"]
-    top = rect["top"]
-    right = left + rect["width"]
-    bottom = top + rect["height"]
-    return ((left), (top),(right),(bottom))
-    #return render_template("Upload.html")
 
+@app.route("/Upload")
+def back():
+    return render_template("Upload.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
